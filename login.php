89b2b5de1edbddaddf2fb9d893869900f4f0c66a
PalpatinChip <ivovamez135@gmail.com>
2022-03-20 11:08:10 +0300

    a

--- login.php
+++ login.php
@@ -1,36 +1,40 @@
 <?php
 require_once 'login_auth.php';
 $conn = new mysqli($hn, $un, $pw, $db);
-if ($conn->connect_error) die($conn->connect_error);
+if ($conn->connect_error)
+    die($conn->connect_error);
 
-if (isset($_POST['name']) && isset($_POST['password'])){
+if (isset($_POST['name']) && isset($_POST['password'])) {
     $name = get_post($conn, 'name');
     $password = get_post($conn, 'password');
     $query = "SELECT name, password FROM register";
     $result = $conn->query($query);
-    if (!$result) echo "Сбой при вставке данных: $query<br>" .
-        $conn->error . "<br><br>";
+    if (!$result)
+        echo "Сбой при вставке данных: $query<br>" .
+            $conn->error . "<br><br>";
 }
 $logged = 0;
 $rows = $result->num_rows;
-for ($j = 0 ; $j < $rows ; ++$j) {
+for ($j = 0; $j < $rows; ++$j) {
     $result->data_seek($j);
     $row = $result->fetch_array(MYSQLI_NUM);
-    if (($name == $row[0]) && ($password == $row[1])) $logged = 1;
+    if (($name == $row[0]) && ($password == $row[1]))
+        $logged = 1;
 }
 $out = $logged == 1 ? '' : "Неправильно введены данные";
 if ($logged == 1) {
     $query = "SELECT * FROM register";
     $result = $conn->query($query);
-    if (!$result) echo "Сбой при вставке данных: $query<br>" .
-        $conn->error . "<br><br>";
+    if (!$result)
+        echo "Сбой при вставке данных: $query<br>" .
+            $conn->error . "<br><br>";
     $rows = $result->num_rows;
-    for ($j = 0 ; $j < $rows ; ++$j)
-    {
-    $result->data_seek($j);
-    $row = $result->fetch_array(MYSQLI_NUM);
-    if ($name == $row[0] && $password == $row[2]) $result = shell_exec("C:\Users\Олег\AppData\Local\Programs\Python\Python39\python.exe C:/Users/Олег/Desktop/Hackathon_1case/hackathon/main.py $name");
-  }
+    for ($j = 0; $j < $rows; ++$j) {
+        $result->data_seek($j);
+        $row = $result->fetch_array(MYSQLI_NUM);
+        if ($name == $row[0] && $password == $row[2])
+            $result = shell_exec("C:\Users\Олег\AppData\Local\Programs\Python\Python39\python.exe C:/Users/Олег/Desktop/Hackathon_1case/hackathon/main.py $name");
+    }
     echo <<<_end
     <!DOCTYPE html>
     <html lang="ru">
@@ -84,7 +88,8 @@ _end;
 
 }
 
-else echo <<<_END
+else
+    echo <<<_END
 <!DOCTYPE html>
 <html lang="ru">
 <head>
@@ -92,16 +97,36 @@ else echo <<<_END
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Login page</title>
     <link rel="stylesheet" type="text/css" href="loginStyle.css">
+    <link rel="stylesheet" type="text/css" href="preloader.css">
 </head>
 <body>
-    <img src = "images/figure.png" alt = "AAA">
+    <script>
+        alert("Такой пользователь не найден. Введите, пожалуйста, заново.")
+    </script>
+    <!-- Preloader -->
+    <div class = "preloader">
+        <div class="sk-cube-grid">
+            <div class="sk-cube sk-cube1"></div>
+            <div class="sk-cube sk-cube2"></div>
+            <div class="sk-cube sk-cube3"></div>
+            <div class="sk-cube sk-cube4"></div>
+            <div class="sk-cube sk-cube5"></div>
+            <div class="sk-cube sk-cube6"></div>
+            <div class="sk-cube sk-cube7"></div>
+            <div class="sk-cube sk-cube8"></div>
+            <div class="sk-cube sk-cube9"></div>
+        </div>
+    </div>
+
+    <div>
+    <img src = "images/figure.png" alt = "AAA" class = "Figures">
     <div class = "Contact_us">
         <h1 class = "Contact_us_text">Contact us:</h1>
         <div class = "contactsBlock">
-            <a class = "contactsImages" style = "background-image: url('images/vk.png');"    href="https://vk.com/ddosguard"  target="_blank"></a>
-            <a class = "contactsImages" style = "background-image: url('images/teleg.png');" href="https://t.me/ddos_guard"   target="_blank"></a>
-            <a class = "contactsImages" style = "background-image: url('images/gmail.png');" href="info@ddos-guard.net"       target="_blank"></a>
-            <a class = "contactsImages" style = "background-image: url('images/wapp.png');"  href="https://wa.me/89889476904" target="_blank"></a>
+            <a class = "contactsImages"  href="https://vk.com/ddosguard"  target="_blank"><img src="images/vk.png"  class= "contactsImages" alt=""></a>
+            <a class = "contactsImages"  href="https://t.me/ddos_guard"   target="_blank"><img src="images/teleg.png"  class= "contactsImages" alt=""></a>
+            <a class = "contactsImages"  href="info@ddos-guard.net"       target="_blank"><img src="images/gmail.png"  class= "contactsImages" alt=""></a>
+            <a class = "contactsImages"  href="https://wa.me/89889476904" target="_blank"><img src="images/wapp.png"  class= "contactsImages" alt=""></a>
         </div>
     </div>
 
@@ -113,14 +138,24 @@ else echo <<<_END
                 <input class = "buttonEnter" type="submit" value = "Enter">
         </form>
         <h1 class = "OR">OR:</h1>
-        <a href="https://" class = "googleEnter" target="_blank"></a>
+        <a href="https://" target="_blank"><img src="images/Google.png" class = "googleEnter"></a>
     </div>
     <div class = "Register">
         <h1 class = "RegAsking">Don't have an account? Lets fix this trouble!</h1>
-        <a href = "reg.html" class = "registerbutton"><h1 class = "toRegBtnText">To registration</h1></a>
+        <a href = "reg.html"><div class = "registerbutton"><h1 class = "toRegBtnText">To registration</h1></div></a>
     </div>
-    <a href="https://ddos-guard.net" class = "Ddos"></a>
-    <div class = "okrug2"></div>
+    <a href="https://ddos-guard.net" class = "Ddos"><img src = "images/ddos.png"> </a>
+    <img src = "images/topright.png" class = "okrug2">
+</div>
+<script>
+    window.onload = function () {
+      document.body.classList.add('loaded_hiding');
+      window.setTimeout(function () {
+        document.body.classList.add('loaded');
+        document.body.classList.remove('loaded_hiding');
+      }, 500);
+    }
+  </script>
 </body>
 </html>
 _END;
